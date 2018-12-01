6fbc082f0cff3db20c350e04e7b33e0fa1b1727c
707130132 <707130132@qq.com>
2018-12-01 22:14:04 +0800

    5times

---
+++ src/test/java/com/king/test/test1/Case1Test.java
@@ -0,0 +1,36 @@
+package com.king.test.test1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//import com.meituan.invoice.thrift.InvoiceCustomerThriftService;
+//import com.meituan.invoice.thrift.TaxDiskBasicResponse;
+//import com.meituan.invoice.thrift.TaxDiskInfoGetRequest;
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Case1Test {
+	
+//	@Autowired
+//	private InvoiceCustomerThriftService.Iface invoiceCustomerThriftService;
+	
+	
+	
+	@Test
+	public void testATest() {
+//		TaxDiskInfoGetRequest taxDiskInfo = new TaxDiskInfoGetRequest();
+//		taxDiskInfo.setCompanyCode("123456789654978654");
+//		
+//		TaxDiskBasicResponse taxDiskBasicResponse= invoiceCustomerThriftService.addTaxDiskInfo(taxDiskInfo);
+//		taxDiskBasicResponse.getCode();
+//		taxDiskBasicResponse.getMsg();
+		
+//		TaxDiskBasicResponse AA = invoiceCustomerThriftService.addTaxDiskInfo(taxDiskInfo);
+//		AA.getCode();
+//		AA.getMsg();
+		System.out.println("AFLKJDFFSLDKFJ");
+	}
+}
