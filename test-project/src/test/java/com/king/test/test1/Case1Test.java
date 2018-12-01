685bb067aecea2fea4465f290660cc44ff9b9f29
707130132 <707130132@qq.com>
2018-12-01 21:02:46 +0800

    2times

--- test-project/src/test/java/com/king/test/test1/Case1Test.java
+++ test-project/src/test/java/com/king/test/test1/Case1Test.java
@@ -6,27 +6,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.meituan.invoice.thrift.InvoiceCustomerThriftService;
-import com.meituan.invoice.thrift.TaxDiskBasicResponse;
-import com.meituan.invoice.thrift.TaxDiskInfoGetRequest;
+//import com.meituan.invoice.thrift.InvoiceCustomerThriftService;
+//import com.meituan.invoice.thrift.TaxDiskBasicResponse;
+//import com.meituan.invoice.thrift.TaxDiskInfoGetRequest;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Case1Test {
 	
-	@Autowired
-	private InvoiceCustomerThriftService.Iface invoiceCustomerThriftService;
+//	@Autowired
+//	private InvoiceCustomerThriftService.Iface invoiceCustomerThriftService;
 	
 	
 	
 	@Test
 	public void testATest() {
-		TaxDiskInfoGetRequest taxDiskInfo = new TaxDiskInfoGetRequest();
-		taxDiskInfo.setCompanyCode("123456789654978654");
-		
-		TaxDiskBasicResponse taxDiskBasicResponse= invoiceCustomerThriftService.addTaxDiskInfo(taxDiskInfo);
-		taxDiskBasicResponse.getCode();
-		taxDiskBasicResponse.getMsg();
+//		TaxDiskInfoGetRequest taxDiskInfo = new TaxDiskInfoGetRequest();
+//		taxDiskInfo.setCompanyCode("123456789654978654");
+//		
+//		TaxDiskBasicResponse taxDiskBasicResponse= invoiceCustomerThriftService.addTaxDiskInfo(taxDiskInfo);
+//		taxDiskBasicResponse.getCode();
+//		taxDiskBasicResponse.getMsg();
 		
 //		TaxDiskBasicResponse AA = invoiceCustomerThriftService.addTaxDiskInfo(taxDiskInfo);
 //		AA.getCode();
